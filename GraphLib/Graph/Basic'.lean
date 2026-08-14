11cf8f40cb2a70fc44ed94199757c6be25788338
Sorrachai Yingchareonthawornchai <sorrachai.cp@gmail.com>
2026-08-14 14:50:16 +0200

    update

--- GraphLib/Graph/Basic'.lean
+++ GraphLib/Graph/Basic'.lean
@@ -5,21 +5,17 @@ Authors: Basil Rohner, Sorrachai Yingchareonthawornchai
 -/
 import Mathlib.Data.Sym.Sym2
 import Cslib.Foundations.Semantics.LTS.Basic
-import Cslib.Foundations.Semantics.LTS.Notation  -- for `s [μ]→ t`
 
-/-!
--/
 
 namespace GraphLib
 variable {α β : Type*}
 
 structure SimpleGraph (α : Type*) where
-  /-- The vertex set. -/
   vertexSet : Set α
   Adj : α → α → Prop
   symm : Std.Symm Adj := by grind
   loopless : Std.Irrefl Adj := by grind
-  left_mem_of_adj : ∀ ⦃x y⦄, Adj x y → x ∈ vertexSet := by grind
+  incidence_left : ∀ ⦃x y⦄, Adj x y → x ∈ vertexSet := by grind
 
 theorem SimpleGraph.adj_symm (G : SimpleGraph α) : Symmetric G.Adj :=
   fun _ _ h => G.symm.symm _ _ h
@@ -28,11 +24,10 @@ def SimpleGraph.edgeSet (G : SimpleGraph α) : Set (Sym2 α) :=
   Sym2.fromRel (G.adj_symm)
 
 structure SimpleDiGraph (α : Type*) where
-  /-- The vertex set. -/
   vertexSet : Set α
   Adj : α → α → Prop
   loopless : Std.Irrefl Adj := by grind
-  mem_of_adj : ∀ ⦃x y⦄, Adj x y → x ∈ vertexSet ∧ y ∈ vertexSet := by grind
+  incidence : ∀ ⦃x y⦄, Adj x y → x ∈ vertexSet ∧ y ∈ vertexSet := by grind
 
 def SimpleDiGraph.edgeSet (G : SimpleDiGraph α) : Set (α × α) :=  {p | G.Adj p.1 p.2}
 
